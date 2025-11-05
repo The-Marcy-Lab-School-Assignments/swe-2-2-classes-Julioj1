dae941e604c90748faf60aef4e0fa848ea8ef1d3
Julioj1 <dbjulioj@gmail.com>
2025-11-05 16:00:22 -0500

    Completed class Rectangle

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,5 +1,21 @@
 class Rectangle {
-
+  constructor(length, width) {
+    this.length = length;
+    this.width = width;
+  }
+  getArea() {
+    return this.length * this.width;
+  }
+  getPerimeter() {
+    return this.length * 2 + this.width * 2;
+  }
+  isSquare() {
+    if (this.length == this.width) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
 
 class Vehicle {
